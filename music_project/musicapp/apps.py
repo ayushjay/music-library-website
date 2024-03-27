f167e5fda7cf0565aaee574406566beab6ffd2c7
ayushjay <ayushtime@gmail.com>
2024-03-27 14:02:45 +0530

    added app

--- music_project/musicapp/apps.py
+++ music_project/musicapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MusicappMainConfig(AppConfig):
+class MusicappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "musicapp_main"
+    name = "musicapp"
