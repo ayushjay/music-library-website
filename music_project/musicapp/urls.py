886bbd34c05cf888b11ea0b8bab824e2447f6c12
ayushjay <ayushtime@gmail.com>
2024-03-27 14:37:49 +0530

    added urls

--- music_project/musicapp/urls.py
+++ music_project/musicapp/urls.py
@@ -1,4 +1,24 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from .views import (
+    SongCreateView,
+    SongDetailView,
+    SongListView,
+    SongDeleteView,
+)
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = "blog"
+
+urlpatterns = [
+    path("", RedirectView.as_view(url="http://localhost:8000/songs/"), name="home"),
+    path("songs/", SongListView.as_view(), name="song-list"),
+    path("create/", SongCreateView.as_view(), name="song-create"),
+    path("songs/<int:pk>", SongDetailView.as_view(), name="song-detail"),
+    path("delete/<int:pk>/", SongDeleteView.as_view(), name="song-delete"),
+]
 
 
-urlpatterns = []
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
